2c4f254c01356dc6aa7adc5ce554a3b4ed8a1493
237480201082_PYTHON <huatringuyen345@gmail.com>
2025-10-19 12:47:02 +0700

    ngay 19 bai tap dau tien

---
+++ bai1_tr49.py
@@ -0,0 +1,16 @@
+print("Tính diện tích đường tròn tương ứng.")
+r = float(input("Nhập vào bán kính đường tròn r: "))
+
+pi = 3.14
+cv = 2 * pi * r
+dt = pi * r * r
+
+print("Chu vi đường tròn là:", cv)
+print("Diện tích đường tròn là:", dt)
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/huatringuyen345-beep/237480201082_17DCNTT2_BAITAP.git
+git push -u origin main
